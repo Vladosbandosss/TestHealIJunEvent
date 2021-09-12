22e1bad35530c8934f6d55a8ca41f3aac57f4dc7
Vladosbandosss <dasistperfektos@gmail.com>
2021-09-12 22:20:04 +0300

    TryEvent

---
+++ Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _currentHealth = 50;
+    private float _maxHealth = 100;
+    private float _minHealth = 0;
+    private float _damage = 10f;
+    private float _heal = 10f;
+    public float CurrentHealth => _currentHealth;
+    public float MaxHealth => _maxHealth;
+    public float MinHealth => _minHealth;
+
+    public delegate void ChangeHealth();
+    public static event ChangeHealth PlayerChangedHealth;
+
+    public void Heal()
+    {
+        _currentHealth += _heal;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+       
+        PlayerChangedHealth();
+    }
+    public void Damage()
+    {
+        _currentHealth -= _damage;
+
+        if (_currentHealth < _minHealth)
+        {
+            _currentHealth = _minHealth;
+        }
+      
+        PlayerChangedHealth();
+    }
+}
---
+++ Scripts/SliderHeal.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHeal : MonoBehaviour
+{
+    [SerializeField] private Slider _healthbar;
+    [SerializeField] private Player _player;
+
+    private float _health;
+    private float _smothing = 5f;
+
+    private void Awake()
+    {
+        _health = _player.CurrentHealth;
+        _healthbar.maxValue = _player.MaxHealth;
+        _healthbar.value = _health;
+        _healthbar.minValue = _player.MinHealth;
+    }
+
+    private void OnEnable()
+    {
+       Player.PlayerChangedHealth += ChangeInfo; 
+    }
+    private void OnDisable()
+    {
+       Player.PlayerChangedHealth -= ChangeInfo;
+    }
+    private void Update()
+    {
+       ShowInfo();
+    }
+    private void ShowInfo()
+    { 
+
+        if (_healthbar.value != _health)
+        {
+
+           _healthbar.value = Mathf.Lerp(_healthbar.value, _health, _smothing * Time.deltaTime);
+
+        }
+    }
+    
+    private void ChangeInfo()
+    {
+        _health = _player.CurrentHealth;
+    }
+}
